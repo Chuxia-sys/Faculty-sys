2545dd3d42d5143743f41c0f872d1efe3f00d2dd
Chuxia-sys <shiela.jkb@gmail.com>
2026-05-14 20:02:57 +0800

    Initial clean commit - New project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black">
+      <h1 className="text-4xl font-bold text-white">
+        Tailwind is working!
+      </h1>
+    </div>
+  )
+}
+
+
+export default App
